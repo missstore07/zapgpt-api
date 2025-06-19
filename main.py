14e41a07ec82037878e5f08c7f61ba30433d8619
missstore07 <missstore07@gmail.com>
2025-06-19 19:25:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -15,4 +15,6 @@ def zapbot():
     )
     return jsonify({"reply": resposta['choices'][0]['message']['content']})
 
-app.run(host="0.0.0.0", port=8080)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
